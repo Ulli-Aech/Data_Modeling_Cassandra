4e7e7b346591f688b528969ef4f7ef5ed70f15ca
Ulli-H <uanklam@yahoo.de>
2020-12-09 12:05:16 +0100

    updating queries

--- cql_queries.py
+++ cql_queries.py
@@ -14,22 +14,22 @@ drop_username_song = ("DROP TABLE IF EXISTS username_song")
 # CREATE TABLES
 
 create_artist_songs = ("""CREATE TABLE IF NOT EXISTS artist_songs 
-                             (sessionId varchar, itemInSession varchar, artist text, 
-                             song_title text, songs_length varchar, 
+                             (sessionId int, itemInSession int, artist text, 
+                             song_title text, songs_length float, 
                              PRIMARY KEY((sessionId, itemInSession), artist,
                              song_title, songs_length))
 """)
 
-craete_artist_song_username = ("""CREATE TABLE IF NOT EXISTS artist_song_username
-                                     (session_id varchar, itemInSession varchar,
-                                      user_id varchar, firstname_user text, lastname_user text,
+create_artist_song_username = ("""CREATE TABLE IF NOT EXISTS artist_song_username
+                                     (user_id int, session_id int, itemInSession int,
+                                      firstname_user text, lastname_user text,
                                       artist text, song_title text,
-                                      PRIMARY KEY ((session_id, user_id), itemInSession))
+                                      PRIMARY KEY ((user_id, session_id), itemInSession))
 """)
 
 create_username_song = ("""CREATE TABLE IF NOT EXISTS username_song 
-                              (song_title text, session_id varchar,
-                               itemInSession varchar, firstname_user text,
+                              (song_title text, session_id int,
+                               itemInSession int, firstname_user text,
                                lastname_user text,
                                PRIMARY KEY ((song_title), session_id, itemInSession))
 """)
@@ -40,8 +40,8 @@ insert_artist_songs = ("""INSERT INTO artist_songs (sessionId, itemInSession, ar
                                 song_title, songs_length)VALUES (%s, %s, %s, %s, %s)
 """)
 
-insert_artist_song_username = ("""INSERT INTO artist_song_username (session_id,
-                                    itemInSession, user_id, firstname_user, 
+insert_artist_song_username = ("""INSERT INTO artist_song_username (user_id, 
+                                    session_id, itemInSession, firstname_user, 
                                     lastname_user, artist, song_title)
                                     VALUES (%s, %s, %s, %s, %s, %s, %s) 
 """)
@@ -53,11 +53,17 @@ insert_username_song = ("""INSERT INTO username_song (song_title, session_id,
 
 # SELECT 
 
-query1 =
+query1 = """SELECT artist, song_title, songs_length
+             FROM artist_songs
+             WHERE sessionId = 338 AND itemInSession = 4"""
 
-query2 =
+query2 = """SELECT artist, song_title, firstname_user, lastname_user 
+             FROM artist_song_username
+             WHERE user_id = 10 AND session_id = 182 """
 
-query3 =
+query3 = """SELECT firstname_user, lastname_user 
+             FROM username_song 
+             WHERE song_title = 'All Hands Against His Own' """
 
 # LISTS OF QUERIES 
 
